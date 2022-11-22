64ce1e1bae3946ffc68729998ebb577df52ccbbb
maher mansour <105998561+mahermansour47455@users.noreply.github.com>
2022-11-22 19:52:39 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EquipeComponent } from './equipe/equipe.component';
+import { AddEquipeComponent } from './add-equipe/add-equipe.component';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { UpdateEquipeComponent } from './update-equipe/update-equipe.component';
+import { LoginComponent } from './login/login.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EquipeComponent,
+    AddEquipeComponent,
+    UpdateProduitComponent,
+    UpdateEquipeComponent,
+    LoginComponent,
+    ForbiddenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
